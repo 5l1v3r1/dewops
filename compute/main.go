82ecc348cac5ef54f5db5f21f590a12f1195b9bc
Boran <boranyldrm21@gmail.com>
2020-03-01 21:00:06 -0800

    panic removed

--- compute/main.go
+++ compute/main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/streadway/amqp"
 	"html"
 	"log"
 	"net/http"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		// Exit the program.
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
 func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@172.18.0.2:5672/")
+	failOnError(err, "Error connecting to the broker")
+	// Make sure we close the connection whenever the program is about to exit.
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	// Make sure we close the channel whenever the program is about to exit.
+	defer ch.Close()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
